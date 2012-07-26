f2deea5c9c81e17b9a6eb213a2f5538c68869eb1
Jessica McKellar <jesstess@mit.edu>
2012-07-26 09:25:21 -0400

    Add example solutions for the suggested exercises.

---
+++ jeopardy_category_clues.DONE.py
@@ -0,0 +1,18 @@
+from pysqlite2 import dbapi2 as sqlite
+
+connection = sqlite.connect('jeopardy.db')
+cursor = connection.cursor()
+
+cursor.execute("SELECT id, name FROM category ORDER BY RANDOM() LIMIT 1")
+results = cursor.fetchall()
+category_id, name = results[0]
+print name
+
+query = """SELECT value, text FROM clue
+WHERE category=%s ORDER BY value""" % (category_id,)
+cursor.execute(query)
+results = cursor.fetchall()
+
+for result in results:
+    value, text = result
+    print "[$%s] %s" % (value, text)
